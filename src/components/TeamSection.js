eb0551276531fea6e97aa3eca87e74571456a9c6
Felipe Pontes <felipemfpontes@gmail.com>
2018-08-05 20:04:38 -0300

    Update to load members from json

--- src/components/TeamSection.js
+++ src/components/TeamSection.js
@@ -1,5 +1,38 @@
 import React from 'react'
 
+import members from '../assets/members.json'
+
+const SocialLink = ({name, url}) => {
+  if (url === null) {
+    return null
+  }
+
+  return (
+    <li className="list-inline-item">
+      <a href={url}>
+        <i className={`fa fa-${name}`}></i>
+      </a>
+    </li>
+  )
+}
+
+const SocialLinks = ({links}) => (
+  <ul className="list-inline social-buttons">
+    {Object.keys(links).map(((social, index) => <SocialLink key={index} name={social} url={links[social]} />)).filter(Boolean)}
+  </ul>
+)
+
+const Member = ({name, description, image, ...socialLinks}) => (
+  <div className="col-sm-3">
+    <div className="team-member">
+      <img className="mx-auto rounded-circle" src={image} alt=""/>
+      <h4>{name}</h4>
+      <p className="text-muted">{description}</p>
+      <SocialLinks links={socialLinks} />
+    </div>
+  </div>
+)
+
 const TeamSection = () => {
   return (
     <section className="bg-light" id="team">
@@ -10,370 +43,8 @@ const TeamSection = () => {
             <h3 className="section-subheading text-muted">Participantes que contribuem para o desenvolvimento da célula acadêmica</h3>
           </div>
         </div>
-        <div className="row">
-
-          <div className="col-sm-3">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/1.jpg" alt=""/>
-              <h4>David Allysson</h4>
-              <p className="text-muted">Desenvolvedor</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="https://github.com/davidallysson">
-                    <i className="fa fa-github"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://facebook.com/david.allysson.9">
-                    <i className="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://linkedin.com/in/david-allysson-527791118/">
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="col-sm-3">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/2.jpg" alt=""/>
-              <h4>Esther Aragão</h4>
-              <p className="text-muted">Desenvolvedora</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="https://github.com/estheraragaos">
-                    <i className="fa fa-github"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://facebook.com/esther.aragao.3">
-                    <i className="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="col-sm-3">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/3.jpeg" alt=""/>
-              <h4>Francisco Bento</h4>
-              <p className="text-muted">Desenvolvedor</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="https://github.com/chicobentojr">
-                    <i className="fa fa-github"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://facebook.com/chicobentojr">
-                    <i className="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://linkedin.com/in/chicobentojr/">
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="col-sm-3">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/4.jpg" alt=""/>
-              <h4>Rafaela Medeiros</h4>
-              <p className="text-muted">Desenvolvedora</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="https://github.com/RafaelaMicaela">
-                    <i className="fa fa-github"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://facebook.com/rafaela.mikaela.7">
-                    <i className="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-        </div>
-
-        <div className="row">
-
-          <div className="col-sm-3">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/5.jpg" alt=""/>
-              <h4>Felipe Pontes</h4>
-              <p className="text-muted">Desenvolvedor</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="https://github.com/felipemfp">
-                    <i className="fa fa-github"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://facebook.com/felipemfp">
-                    <i className="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="col-sm-3">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/6.jpg" alt=""/>
-              <h4>Dayanne Morato</h4>
-              <p className="text-muted">Desenvolvedora</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="https://github.com/DayanneMorato">
-                    <i className="fa fa-github"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://facebook.com/dayanne.dealmeidamorato">
-                    <i className="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="col-sm-3">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/7.jpg" alt=""/>
-              <h4>Herikle Mesquita</h4>
-              <p className="text-muted">Desenvolvedor</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="https://github.com/Herikle">
-                    <i className="fa fa-github"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://facebook.com/herikle.mesquita">
-                    <i className="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="col-sm-3">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/8.jpg" alt=""/>
-              <h4>Yuri Sales</h4>
-              <p className="text-muted">Desenvolvedor</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="https://github.com/yuriscosta">
-                    <i className="fa fa-github"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://facebook.com/yurishenrique">
-                    <i className="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-        </div>
-
-        <div className="row">
-
-          <div className="col-sm-3">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/9.jpg" alt=""/>
-              <h4>Albert Morato</h4>
-              <p className="text-muted">Desenvolvedor</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="https://github.com/albert06">
-                    <i className="fa fa-github"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://facebook.com/albert.dealmeidamorato">
-                    <i className="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="col-sm-3">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/10.jpg" alt=""/>
-              <h4>Gésio de Moura</h4>
-              <p className="text-muted">Desenvolvedor</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="https://github.com/srgesio">
-                    <i className="fa fa-github"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://facebook.com/srgesio">
-                    <i className="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="col-sm-3">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/11.jpg" alt=""/>
-              <h4>Helenita Gurgel</h4>
-              <p className="text-muted">Desenvolvedora</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="https://github.com/helenitagurgel">
-                    <i className="fa fa-github"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://facebook.com/helenitagurgel">
-                    <i className="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="col-sm-3">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/12.jpg" alt="" />
-              <h4>Yves Cabral</h4>
-              <p className="text-muted">Desenvolvedor</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="https://github.com/yvescabral">
-                    <i className="fa fa-github"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://facebook.com/yvescabral">
-                    <i className="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-        </div>
-
-        <div className="row">
-
-          <div className="col-sm-6">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/13.jpg" alt=""/>
-              <h4>Vitor Souza</h4>
-              <p className="text-muted">Desenvolvedor</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-github"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://facebook.com/pvitorzero">
-                    <i className="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="col-sm-6">
-            <div className="team-member">
-              <img className="mx-auto rounded-circle" src="img/team/14.jpg" alt=""/>
-              <h4>Astrogilda Caroline</h4>
-              <p className="text-muted">Desenvolvedora</p>
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="https://github.com/AstroCarol">
-                    <i className="fa fa-github"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="https://facebook.com/astrogilda.carol">
-                    <i className="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
+        <div className="row" style={{justifyContent: "space-evenly"}}>
+          {members.map((member, index) => <Member key={index} {...member} />)}
         </div>
       </div>
     </section>
